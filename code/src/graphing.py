8a9aedf896577b8d4a08ec71bbfcf4ef77a88559
Ted Tooth <tooth.ted@gmail.com>
2023-08-20 15:13:54 +0100

    Added ability to change which column is used for color generation.

--- code/src/graphing.py
+++ code/src/graphing.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-def PlotBayOps(D):
+def PlotBayOps(D, color_col=None):
     D["vp"] = 'Vehicle: ' +\
         D.v.astype(str) +\
         ' Procedure: ' +\
@@ -11,31 +11,20 @@ def PlotBayOps(D):
         ' Operation: ' +\
         D.o.astype(str)
 
-    # Group adjacent vehicle procedures (b).
-    B = D.groupby('b',
-                  as_index=False,
-                  group_keys=False)
+    # Which column to use as the color gradient (g).
+    g = 'o' if color_col is None else color_col
 
     _D = D.copy()
-    
-    n_oc = len(_D.oc.unique()) + 1
-    cmap = plt.get_cmap("gist_rainbow", n_oc)
+
+    uniques = _D[g].unique()
+    M = dict(zip(uniques, range(len(uniques))))
+    _D['i_g'] = _D[g].astype(int).map(M)
+
+    n = len(_D[g].unique()) + 1
+    cmap = plt.get_cmap("gist_rainbow", n)
     palette = [mpl.colors.rgb2hex(cmap(i)) for i in range(cmap.N)]
-    
-    _D['color'] = _D.oc.astype(int).map(pd.Series(palette))
-    
-    # for _, b in B:
-    #     # Find the number of operation clusters per bay.
-    #     n_oc = len(b.oc.unique()) + 1
-    #     cmap = plt.get_cmap("gist_rainbow", n_oc)
-    #     custom_palette = [mpl.colors.rgb2hex(cmap(i)) for i in range(cmap.N)]
 
-    #     C = pd.Series(custom_palette)
-    #     print(C)
-    #     _D = D.copy()
-    #     _D['color'] = _.oc.astype(int).map(C)
-
-    #     print(_D)
+    _D['color'] = _D.i_g.astype(int).map(pd.Series(palette))
 
     plt.barh(y=_D.vp,
              width=_D.d,
